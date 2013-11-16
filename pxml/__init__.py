201985f48733af9bd0ac068105c545bfa35111ad
metagriffin <mg.github@uberdev.org>
2013-11-16 18:17:14 -0500

    added preamble

--- pxml/__init__.py
+++ pxml/__init__.py
@@ -1,9 +1,22 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #------------------------------------------------------------------------------
-# file: $Id: pxml.py 346 2012-08-12 17:22:39Z griffin $
-# auth: metagriffin <metagriffin@uberdev.org>
+# file: $Id$
+# auth: metagriffin <mg.github@uberdev.org>
 # date: 2009/06/05
-# copy: (C) CopyLoose 2009 UberDev <hardcore@uberdev.org>, No Rights Reserved.
+# copy: (C) Copyright 2009-EOT metagriffin -- see LICENSE.txt
+#------------------------------------------------------------------------------
+# This software is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
 #------------------------------------------------------------------------------
 
 '''
