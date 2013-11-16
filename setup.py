99e364ced0c63ad0562c0ebd4550a0f965942b4b
metagriffin <mg.github@uberdev.org>
2013-11-16 18:18:12 -0500

    added preamble

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #------------------------------------------------------------------------------
-# file: $Id: setup.py 346 2012-08-12 17:22:39Z griffin $
-# desc: the pxml installation file
-# auth: metagriffin <metagriffin@uberdev.org>
+# file: $Id$
+# auth: metagriffin <mg.github@uberdev.org>
 # date: 2010/12/21
-# copy: (C) CopyLoose 2010 UberDev <hardcore@uberdev.org>, No Rights Reserved.
+# copy: (C) Copyright 2013-EOT metagriffin -- see LICENSE.txt
+#------------------------------------------------------------------------------
+# This software is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
 #------------------------------------------------------------------------------
 
 import sys, os, re, setuptools
